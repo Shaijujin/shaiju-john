6cf8094bea31b2af7a7732359d840c8acc0c39af
shaiju jin <shaijujcompunet@solutions>
2024-04-20 11:11:26 +0530

    secound commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,6 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import EmailIcon from '@mui/icons-material/Email';
 import { Bio } from '../../data/constants';
 
 const FooterContainer = styled.div`
